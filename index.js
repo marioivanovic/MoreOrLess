84d6ac40e672accc9a29bb9417a8d8ac84c22e92
mrmarioi <ivanovic.mario@gmail.com>
2022-02-14 14:14:38 +0100

    :heavy_plus_sign: NPM prompt added :heavy_plus_sign:

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-var mysteryNumber = Math.floor((Math.random() * 100) + 1);
-var guessNumber = 30;
+var prompt = require('prompt');
+
+//
+// Start the prompt
+//
+prompt.start();
+console.log('Donnez un chiffre entre 1 et 100 :');
+//
+// Get two properties from the user: username and email
+//
+var random = Math.floor((Math.random() * 100) + 1);
+prompt.get(['number'], function (err, result) {
+    //
+    // Log the results.
+    //
+    console.log('Command-line input received:');
+    console.log('Number choosen: ' + result.number);
+    console.log('Nombre mystère:' + result.random);
+});
+});
